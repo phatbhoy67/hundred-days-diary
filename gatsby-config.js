99dbdaba931af4c00073c6c7fade1ecbabafbc9c
phatbhoy67 <chris.scanlon72@gmail.com>
2018-09-24 17:34:04 +0100

    Added web manifest and icon image
    Installed:
    gatsby-plugin-manifest
    gatsby-plugin-offline
    
    Updated gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: `100 Days of Code Diary`,
+    title: `100 Days of Code Diary`
   },
   pathPrefix: `/hundred-days-diary`,
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "100 Days of Code Diary",
+        short_name: "100DOC",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        display: "minimal-ui",
+        icon: "src/images/icon.png" // This path is relative to the root of the site.
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+        pathToConfigModule: `src/utils/typography`
+      }
     },
-  ],
-}
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`
+  ]
+};
